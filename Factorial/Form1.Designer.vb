b45395b60789db0019d9fd645e080f077823cf1e
Obinna <obinna.achara@outlook.com>
2022-01-22 22:49:48 +0100

    VB.Net Desktop Factorial App

---
+++ Factorial/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btbFactorial = New System.Windows.Forms.Button()
+        Me.txtnumber = New System.Windows.Forms.TextBox()
+        Me.lblresult = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblslimsoft = New System.Windows.Forms.Label()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DarkGoldenrod
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.txtnumber)
+        Me.GroupBox1.Controls.Add(Me.lblresult)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(49, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(293, 263)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnExit)
+        Me.GroupBox2.Controls.Add(Me.btnClear)
+        Me.GroupBox2.Controls.Add(Me.btbFactorial)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 150)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(281, 78)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(200, 29)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(103, 29)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btbFactorial
+        '
+        Me.btbFactorial.Location = New System.Drawing.Point(6, 29)
+        Me.btbFactorial.Name = "btbFactorial"
+        Me.btbFactorial.Size = New System.Drawing.Size(75, 23)
+        Me.btbFactorial.TabIndex = 1
+        Me.btbFactorial.Text = "Fa&ctorial"
+        Me.btbFactorial.UseVisualStyleBackColor = True
+        '
+        'txtnumber
+        '
+        Me.txtnumber.Location = New System.Drawing.Point(94, 41)
+        Me.txtnumber.Name = "txtnumber"
+        Me.txtnumber.Size = New System.Drawing.Size(87, 21)
+        Me.txtnumber.TabIndex = 0
+        '
+        'lblresult
+        '
+        Me.lblresult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblresult.Location = New System.Drawing.Point(94, 91)
+        Me.lblresult.Name = "lblresult"
+        Me.lblresult.Size = New System.Drawing.Size(162, 30)
+        Me.lblresult.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Result"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number"
+        '
+        'lblslimsoft
+        '
+        Me.lblslimsoft.AutoSize = True
+        Me.lblslimsoft.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblslimsoft.Location = New System.Drawing.Point(12, 323)
+        Me.lblslimsoft.Name = "lblslimsoft"
+        Me.lblslimsoft.Size = New System.Drawing.Size(51, 13)
+        Me.lblslimsoft.TabIndex = 1
+        Me.lblslimsoft.Text = "Slimsoft"
+        '
+        'Timer
+        '
+        Me.Timer.Enabled = True
+        Me.Timer.Interval = 170
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Goldenrod
+        Me.Label4.Location = New System.Drawing.Point(-1, 317)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(391, 28)
+        Me.Label4.TabIndex = 2
+        '
+        'ErrorProvider
+        '
+        Me.ErrorProvider.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btbFactorial
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SaddleBrown
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(387, 345)
+        Me.Controls.Add(Me.lblslimsoft)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Name = "Form1"
+        Me.Text = "Factorial"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtnumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblresult As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btbFactorial As System.Windows.Forms.Button
+    Friend WithEvents lblslimsoft As System.Windows.Forms.Label
+    Friend WithEvents Timer As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ErrorProvider As System.Windows.Forms.ErrorProvider
+
+End Class
---
+++ Factorial/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtnumber.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+
+        lblslimsoft.Left = lblslimsoft.Left + 5
+
+        If lblslimsoft.Left > Me.Width Then
+            lblslimsoft.Left = 0
+        End If
+
+    End Sub
+
+    Private Sub btbFactorial_Click(sender As Object, e As EventArgs) Handles btbFactorial.Click
+
+        Dim no, factorial As Integer
+
+        no = Val(txtnumber.Text)
+
+        factorial = 1
+
+        For i = no To 1 Step -1
+            On Error Resume Next
+
+            factorial = factorial * i
+
+        Next i
+
+        lblresult.Text = factorial
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtnumber.Text = ""
+        lblresult.Text = ""
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'to close the app
+
+        Dim returndialogresult As System.Windows.Forms.DialogResult
+        Dim title As String = "Sure to exit"
+        Dim message As String = "Do you want to exit?"
+
+        returndialogresult = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+
+        If returndialogresult = Windows.Forms.DialogResult.Yes Then
+
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub txtnumber_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtnumber.Validating
+
+        Dim number As Integer
+
+        Try
+            number = Integer.Parse(txtnumber.Text)
+
+            If txtnumber.Text = String.Empty Then
+
+                'Cancel the event
+                e.Cancel = True
+                txtnumber.SelectAll()
+                txtnumber.Focus()
+                ErrorProvider.SetError(txtnumber, "Number must not be blank")
+
+            End If
+
+        Catch ex As Exception
+            e.Cancel = True
+            txtnumber.SelectAll()
+            txtnumber.Focus()
+            ErrorProvider.SetError(txtnumber, "Nunmer must be numeric")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+        e.Cancel = False
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
